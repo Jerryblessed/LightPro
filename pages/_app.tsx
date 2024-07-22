9fe2c418e9472390db249fbc6fd9d277d978755b
Jeremiah Ope <106873857+Jerryblessed@users.noreply.github.com>
2024-07-23 00:19:41 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,6 @@ import { ThemeProvider } from 'next-themes'
 import Layout from "@/components/Layout";
 import Home from '../app/App';
 import Page from './page'
-import Home2 from '@/src/app/page';
 
 export default function App({ Component, pageProps }: AppProps) {
 
