30a93799fc66f6a9e3a80e02eedcf4096ff81b61
Jeremiah Ope <106873857+Jerryblessed@users.noreply.github.com>
2024-07-23 00:16:41 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes'
 import Layout from "@/components/Layout";
-import Ochat from '../src/App'
 import Home from '../app/App';
 import Page from './page'
 import Home2 from '@/src/app/page';
